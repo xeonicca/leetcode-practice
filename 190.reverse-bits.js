a47b307e56e3fa64cc04244b5f54b1aef9dc198b
Steve Yu <soundedrain@gmail.com>
2022-08-25 09:59:00 +0800

    add explanation to 190

--- 190.reverse-bits.js
+++ 190.reverse-bits.js
@@ -10,15 +10,19 @@
  * @return {number} - a positive integer
  */
 var reverseBits = function (n) {
-  let binary = n.toString(2)
   let res = 0
 
   for (let i = 0; i < 32; i++) {
+    // left shift result by 1
     res <<= 1
+    // get the first bit of n (using logic and 1)
+    // then logic or the result with result (to preserve 0 and 1s)
     res |= n & 1
+    // right shift n by 1 to move on to the next bit
     n >>= 1
   }
 
+  // logic right shift to preserve the signed bit
   return res >>> 0
 }
 // @lc code=end
